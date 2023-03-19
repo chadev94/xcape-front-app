b1868be627d071036c5306d084b621f553103954
kojoo112 <kapella000@gmail.com>
2023-03-19 19:16:38 +0900

    XCAPE 탭 스타일링

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,19 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const navArr = [
-    {merchantName: "건대점", merchantLink: "/ku"},
-    {merchantName: "건대2호점", merchantLink: "/ku2"},
-    {merchantName: "강남점", merchantLink: "/gangnam"},
-    {merchantName: "수원점", merchantLink: "/suwon"},
-    {merchantName: "홍대점", merchantLink: "/hongdae"},
+    { merchantName: "건대점", merchantLink: "/ku" },
+    { merchantName: "건대2호점", merchantLink: "/ku2" },
+    { merchantName: "강남점", merchantLink: "/gangnam" },
+    { merchantName: "수원점", merchantLink: "/suwon" },
+    { merchantName: "홍대점", merchantLink: "/hongdae" },
 ];
 
 function Nav() {
     return (
-        <div
-            className="inline-block text-center whitespace-nowrap py-6 border-b border-zinc-500 w-full overflow-x-auto">
+        <div className="inline-block text-center whitespace-nowrap py-6 border-b border-zinc-500 w-full overflow-x-auto">
             {navArr.map((nav, index) => {
                 return index === 0 ? (
-                    <Link to={nav.merchantLink}>
+                    <Link to={nav.merchantLink} key={index}>
                         <button
                             key={index}
                             className="p-1 sm:p-2 text-lg border border-zinc-500
@@ -25,12 +24,13 @@ function Nav() {
                         </button>
                     </Link>
                 ) : (
-                    <Link className="" to={nav.merchantLink}>
+                    <Link to={nav.merchantLink} key={index}>
                         <button
                             className="p-1 sm:p-2 text-lg border border-zinc-500
                             bg-zinc-800 rounded-sm ml-2
                             text-zinc-400"
-                            key={index}>
+                            key={index}
+                        >
                             {nav.merchantName}
                         </button>
                     </Link>
--- src/components/styled/xcapeStyled.tsx
+++ src/components/styled/xcapeStyled.tsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 import gundaeImage from "../assets/images/merchant/gundae.jpeg";
 
 export const Container = styled.div`
-    color: red;
-    font-size: 25px;
+    //color: red;
+    //font-size: 25px;
 `;
 export const Nav = styled.nav`
-    display: flex;
-    margin-top: 10px;
+    //display: flex;
+    //margin-top: 10px;
 `;
 export const Underline = styled(motion.div)`
     border-bottom: 1px solid;
@@ -17,72 +17,72 @@ export const Underline = styled(motion.div)`
     height: 1vh;
 `;
 export const Brand = styled.div`
-    width: 100%;
-    text-align: center;
-    font-size: 0.6em;
-    color: whitesmoke;
-    cursor: pointer;
+    //width: 100%;
+    //text-align: center;
+    //font-size: 0.6em;
+    //color: whitesmoke;
+    //cursor: pointer;
 `;
 export const Info = styled.div`
-    width: 100%;
-    text-align: center;
-    font-size: 0.6em;
-    color: whitesmoke;
-    cursor: pointer;
+    //width: 100%;
+    //text-align: center;
+    //font-size: 0.6em;
+    //color: whitesmoke;
+    //cursor: pointer;
 `;
 export const Acess = styled.div`
-    width: 100%;
-    text-align: center;
-    font-size: 0.6em;
-    color: whitesmoke;
-    cursor: pointer;
+    //width: 100%;
+    //text-align: center;
+    //font-size: 0.6em;
+    //color: whitesmoke;
+    //cursor: pointer;
 `;
 export const Guide = styled.img<{ cursor?: string }>`
-    width: 100%;
-    cursor: ${(props) => props.cursor};
+    // width: 100%;
+    // cursor: ${(props) => props.cursor};
 `;
 export const BrandWrapper = styled.div`
-    position: relative;
-    width: 100%;
-    height: auto;
-    padding-top: 50%;
+    //position: relative;
+    //width: 100%;
+    //height: auto;
+    //padding-top: 50%;
 `;
 export const InfoWrapper = styled.div`
-    position: relative;
-    width: 100%;
-    height: auto;
+    //position: relative;
+    //width: 100%;
+    //height: auto;
 `;
 export const XcapeImage = styled.img`
-    position: absolute;
-    top: 0;
-    width: 100%;
+    //position: absolute;
+    //top: 0;
+    //width: 100%;
 `;
 export const YoutubeBox = styled.div`
-    margin: 0 5vw;
-    padding-top: 56.25%;
-    position: relative;
+    //margin: 0 5vw;
+    //padding-top: 56.25%;
+    //position: relative;
 `;
 export const AccessWrapper = styled.div`
-    padding: 5% 3%;
+    //padding: 5% 3%;
 `;
 export const MerchantBox = styled.div`
-    width: 100%;
-    background-color: #282828;
+    //width: 100%;
+    //background-color: #282828;
 `;
 export const ImgBox = styled.div`
-    display: flex;
-    justify-content: center;
+    //display: flex;
+    //justify-content: center;
 `;
 export const Image = styled.img`
-    margin: 5%;
-    width: 93%;
-    height: 93%;
+    //margin: 5%;
+    //width: 93%;
+    //height: 93%;
 `;
 export const Name = styled.div`
-    margin-left: 3%;
-    margin-bottom: 3%;
-    font-size: 1em;
-    color: #ffffff;
+    //margin-left: 3%;
+    //margin-bottom: 3%;
+    //font-size: 1em;
+    //color: #ffffff;
 `;
 export const Address = styled.div`
     margin-left: 3%;
