4c1786d6572244e9afd9b52eb25766665ff46caa
Ryu Kyungbin <kyungbin.ryu@linecorp.com>
2023-03-26 20:39:42 +0900

    Add Notion view and loading view

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,44 +1,9 @@
 import "react-notion/src/styles.css";
-import { NotionRenderer } from "react-notion";
-import React, { useState, useEffect } from "react";
+import NotionView from "./NotionView";
 // import fs from "fs";
 
 function Review() {
-    const [response, setResponse] = useState({});
-
-    const divStyle = {
-        WebkitTextFillColor: "white",
-    };
-
-    useEffect(() => {
-        const NOTION_PAGE_ID = "ce039ebcfdea42b3b941528f76c294b9";
-        fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
-            .then((res) => res.json())
-            .then((resJson) => {
-                setResponse(resJson);
-            });
-    }, []);
-
-    // const createNotionJsonFile = () => {
-    //     const NOTION_PAGE_ID = "ce039ebcfdea42b3b941528f76c294b9";
-    //     fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
-    //         .then((res) => res.json())
-    //         .then((resJson) => {
-    //             fs.writeFile("../data/review.json", resJson, (err) => {
-    //                 if (err) {
-    //                     console.log(err);
-    //                     return;
-    //                 }
-    //                 console.log("Json file created successfully");
-    //             });
-    //         });
-    // };
-
-    return (
-        <div style={divStyle}>
-            <NotionRenderer blockMap={response} fullPage={true} />
-        </div>
-    );
+    return <NotionView pageId={"ce039ebcfdea42b3b941528f76c294b9"} />;
 }
 
 export default Review;
