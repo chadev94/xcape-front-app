e936a5116ea8443e18051d57d467fbfb2919f9b8
kojoo112 <kapella000@gmail.com>
2023-04-28 21:09:01 +0900

    notion api xcape에 표시

--- src/components/ThemeDetail.tsx
+++ src/components/ThemeDetail.tsx
@@ -8,7 +8,6 @@ function ThemeDetail() {
     const navigate = useNavigate();
     const { merchantCode } = useParams<{ merchantCode: string }>();
     const [currentTheme, setCurrentTheme] = useState<ITheme>();
-    const book = require("../assets/images/book.png");
     const { themeId } = useParams<{ themeId: string }>();
 
     useEffect(() => {
@@ -41,17 +40,25 @@ function ThemeDetail() {
                             {currentTheme.nameKo}
                         </div>
                         <div className="text-xl text-zinc-500">{currentTheme.nameEn}</div>
-                        <div className="mt-8 py-5 text-center text-white whitespace-pre-wrap">{currentTheme.description}</div>
+                        <div className="mt-8 py-5 text-center text-white whitespace-pre-wrap">
+                            {currentTheme.description}
+                        </div>
                         <div className="w-full flex justify-between items-center">
                             <div className="flex">
                                 <div className="text-2xl" style={{ color: currentTheme.colorCode }}>
                                     난이도
                                 </div>
-                                {makeBooleanArray(currentTheme.difficulty).map((star) => {
+                                {makeBooleanArray(currentTheme.difficulty).map((star, index) => {
                                     if (star) {
-                                        return <Icon.Star style={{ color: currentTheme.colorCode }} className="h-8 w-8 xs:h-10 xs:w-10" />;
+                                        return (
+                                            <Icon.Star
+                                                key={index}
+                                                style={{ color: currentTheme.colorCode }}
+                                                className="h-8 w-8 xs:h-10 xs:w-10"
+                                            />
+                                        );
                                     }
-                                    return <Icon.Star className="text-zinc-600 h-8 w-8 xs:h-10 xs:w-10" />;
+                                    return <Icon.Star key={index} className="text-zinc-600 h-8 w-8 xs:h-10 xs:w-10" />;
                                 })}
                             </div>
                             <div className="text-md lg:text-2xl text-white">
@@ -61,17 +68,30 @@ function ThemeDetail() {
                                 </span>
                             </div>
                         </div>
-                        <div className="grid grid-cols-2 p-3 w-full mb-3" style={{ backgroundColor: currentTheme.colorCode }}>
+                        <div
+                            className="grid grid-cols-2 p-3 w-full mb-3"
+                            style={{ backgroundColor: currentTheme.colorCode }}
+                        >
                             {currentTheme.abilityList.map((ability) => {
                                 return (
                                     <div key={ability.id} className="text-md xs:text-md lg:text-lg lg:text-xl flex">
                                         <div className="text-white">{ability.name}</div>
                                         <div className="flex items-center justify-between w-2/3 sm:w-1/2 ml-2">
-                                            {makeBooleanArray(ability.value).map((item) => {
+                                            {makeBooleanArray(ability.value).map((item, index) => {
                                                 if (item) {
-                                                    return <div className="h-4 w-4 sm:h-3 sm:w-3  bg-white rounded-full"></div>;
+                                                    return (
+                                                        <div
+                                                            key={index}
+                                                            className="h-4 w-4 sm:h-3 sm:w-3  bg-white rounded-full"
+                                                        ></div>
+                                                    );
                                                 }
-                                                return <div className="h-4 w-4 sm:h-3 sm:w-3 bg-black rounded-full"></div>;
+                                                return (
+                                                    <div
+                                                        key={index}
+                                                        className="h-4 w-4 sm:h-3 sm:w-3 bg-black rounded-full"
+                                                    ></div>
+                                                );
                                             })}
                                         </div>
                                     </div>
